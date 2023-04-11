3f3ecab0c5078d5f3439aae116d3fa451c5ddf9d
Ray Phelan <rayphelan@gmail.com>
2023-04-11 13:06:16 +0100

    feat: tests for encode and decode performance fee

--- packages/sdk/src/fees/fees/performanceFee.ts
+++ packages/sdk/src/fees/fees/performanceFee.ts
@@ -1,6 +1,27 @@
-import { encodeAbiParameters } from "viem";
+import type { Hex } from "viem";
+import { decodeAbiParameters, encodeAbiParameters } from "viem";
 import type { Address } from "viem";
 import { ZERO_ADDRESS } from "../../constants/misc.js";
+
+export const performanceFeeSettingsEncoding = [
+  {
+    type: "uint256",
+    name: "feeRate",
+  },
+  {
+    type: "address",
+    name: "feeRecipient",
+  },
+] as const;
+
+export function decodePerformanceFeeSettings(settings: Hex) {
+  const [feeRateInBps, feeRecipient] = decodeAbiParameters(performanceFeeSettingsEncoding, settings);
+
+  return {
+    feeRateInBps,
+    feeRecipient,
+  };
+}
 
 export function encodePerformanceFeeSettings({
   feeRateInBps,
@@ -9,17 +30,5 @@ export function encodePerformanceFeeSettings({
   feeRateInBps: bigint;
   feeRecipient?: Address;
 }) {
-  return encodeAbiParameters(
-    [
-      {
-        type: "uint256",
-        name: "feeRate",
-      },
-      {
-        type: "address",
-        name: "feeRecipient",
-      },
-    ],
-    [feeRateInBps, feeRecipient],
-  );
+  return encodeAbiParameters(performanceFeeSettingsEncoding, [feeRateInBps, feeRecipient]);
 }
