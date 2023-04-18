d87d017e19d77a27374be32c0a20c4fe4296a90f
Ray Phelan <rayphelan@gmail.com>
2023-04-18 11:58:05 +0100

    test: updated test

--- packages/sdk/src/actions/redeemSharesInKind.test.ts
+++ packages/sdk/src/actions/redeemSharesInKind.test.ts
@@ -8,6 +8,9 @@ import {
 import { encodeFunctionData } from "viem";
 import { publicClient, sendTestTransaction, testActions } from "../../tests/globals.js";
 import { ALICE, WETH } from "../../tests/constants.js";
+import { setupAnvil } from "../../tests/anvil.js";
+
+setupAnvil();
 
 test("redeem shares in kind should work correctly", async () => {
   const { comptrollerProxy, vaultProxy } = await testActions.createTestVault({
