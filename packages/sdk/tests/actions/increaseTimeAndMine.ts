6ca2e8c91c4eafc224ca096ff6244f7f6a9ce120
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-18 12:05:13 +0200

    chore: fix

--- packages/sdk/tests/actions/increaseTimeAndMine.ts
+++ packages/sdk/tests/actions/increaseTimeAndMine.ts
@@ -1,14 +1,12 @@
-import { testClient } from "../client.js";
-
-import { testClient } from "../client.js";
+import { testClient } from "../globals.js";
 
 export async function increaseTimeAndMine({ seconds, blocks = 1 }: { seconds: number; blocks?: number }) {
   if (seconds <= 0) {
-    throw new Error('Seconds must be a positive integer');
+    throw new Error("Seconds must be a positive integer");
   }
 
   if (blocks <= 0) {
-    throw new Error('Number of blocks must be a positive integer');
+    throw new Error("Number of blocks must be a positive integer");
   }
 
   await testClient.increaseTime({ seconds });
