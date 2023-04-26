79297e0c9750793b3c18bd76f773e152d15e573d
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-26 20:20:42 +0200

    docs: add api doc tags

--- packages/api/src/client.ts
+++ packages/api/src/client.ts
@@ -4,10 +4,22 @@ import { EnzymeService } from "./protobuf.js";
 
 export type EnzymeClient = PromiseClient<typeof EnzymeService>;
 
+/**
+ * Creates a client for the Enzyme service.
+ *
+ * @param transport The transport to use.
+ * @returns The client.
+ */
 export function createClient(transport: Transport): EnzymeClient {
   return createPromiseClient(EnzymeService, transport);
 }
 
+/**
+ * Creates a token auth interceptor.
+ *
+ * @param token The token to use for authentication.
+ * @returns The interceptor.
+ */
 export function createTokenAuthInterceptor(token: string): Interceptor {
   return (next) => async (req) => {
     req.header.set("Authorization", `Bearer ${token}`);
@@ -20,6 +32,13 @@ export interface TransportOptions {
   interceptors?: Interceptor[];
 }
 
+/**
+ * Decorates transport options with a token auth interceptor.
+ *
+ * @param token The token to use for authentication.
+ * @param options The transport options to decorate.
+ * @returns The decorated transport options.
+ */
 export function withTokenAuth<TOptions extends TransportOptions>(token: string, options: TOptions): TOptions {
   const auth = createTokenAuthInterceptor(token);
   const interceptors = (options.interceptors ?? []).concat(auth);
