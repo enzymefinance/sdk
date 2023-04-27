45c7066cc24b09807a564c357faddd93e35196c5
Ivan Herger <ivan@herger.org>
2023-04-27 10:07:20 +0200

    docs: add documentation for fee settlement types

--- packages/sdk/src/fees/enums.ts
+++ packages/sdk/src/fees/enums.ts
@@ -14,10 +14,32 @@ export const FeeManagerAction = {
 
 export type FeeSettlementType = typeof FeeSettlementType[keyof typeof FeeSettlementType];
 export const FeeSettlementType = {
+  /**
+   * No fees are being paid.
+   */
   None: 0,
+  /**
+   * The fee is paid by transfering vault shares from the depositor to the fee recipient.
+   */
   Direct: 1,
+  /**
+   * The fee is paid by minting new vault shares for the fee recipient.
+   */
   Mint: 2,
+  /**
+   * The fee is paid by burning vault shares of the depositor.
+   */
   Burn: 3,
+  /**
+   * The fee is paid by minting new vault shares and transferring them to the vault itself.
+   * @remarks Only used in v2 and v3. 
+   * @deprecated
+   */
   MintSharesOutstanding: 4,
+  /**
+   * The fee is paid by burning vault shares held by the vault itself.
+   * @remarks Only used in v2 and v3. 
+   * @deprecated
+   */
   BurnSharesOutstanding: 5,
 } as const;
