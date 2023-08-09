7761167bd374321e624a1fd6aa71e1f720fa44d6
Paweł Kędzia <pawelkedzia95@gmail.com>
2023-08-09 13:55:32 +0200

    chore: improve min func

--- packages/sdk/src/utils/math.ts
+++ packages/sdk/src/utils/math.ts
@@ -1,14 +1,6 @@
 export function min(firstValue: bigint, ...restValues: bigint[]) {
-  const values = [firstValue, ...restValues];
-
   let minValue = firstValue;
-
-  for (let i = 1; i < values.length; i++) {
-    const value = values[i];
-    if (value === undefined) {
-      throw new Error("value is undefined");
-    }
-
+  for (const value of restValues) {
     if (value < minValue) {
       minValue = value;
     }
