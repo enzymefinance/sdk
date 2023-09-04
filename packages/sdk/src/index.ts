89849bea53597649f474225cd97492c32f35072d
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-04 09:10:15 +0200

    feat: add vault has reconfiguration signalled (#108)

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -335,6 +335,9 @@ export { getSharesActionTimelock } from "./reads/getSharesActionTimelock.js";
 // ./reads/getTrackedAssets.js
 export { getTrackedAssets } from "./reads/getTrackedAssets.js";
 
+// ./reads/getVaultHasReconfiguration.js
+export { getVaultHasReconfigurationRequest } from "./reads/getVaultHasReconfiguration.js";
+
 // ./reads/getVaultName.js
 export { getVaultName } from "./reads/getVaultName.js";
 
