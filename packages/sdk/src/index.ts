891ba9d07ba0d0a0af16e494f964db6b6132d4de
KedziaPawel <67273035+KedziaPawel@users.noreply.github.com>
2023-09-05 08:45:21 +0200

    feat: get expected shares for shares wrapper (#110)
    Co-authored-by: Sebastian Lorenz <fubhy@fubhy.com>

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -275,6 +275,9 @@ export { getEntranceFeeRate } from "./reads/getEntranceFeeRate.js";
 // ./reads/getExitFeeRates.js
 export { getExitFeeRates } from "./reads/getExitFeeRates.js";
 
+// ./reads/getExpectedSharesForGatedRedemptionQueueSharesWrapper.js
+export { getExpectedSharesForGatedRedemptionQueueSharesWrapper } from "./reads/getExpectedSharesForGatedRedemptionQueueSharesWrapper.js";
+
 // ./reads/getExternalPositionAssets.js
 export { getExternalPositionAssets } from "./reads/getExternalPositionAssets.js";
 
