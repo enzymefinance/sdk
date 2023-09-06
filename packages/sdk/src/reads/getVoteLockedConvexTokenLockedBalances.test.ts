1416db3ebf0eb8b4489411d61c0de70fca44d8f8
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-06 14:41:51 +0200

    feat: update test to check result types

--- packages/sdk/src/reads/getVoteLockedConvexTokenLockedBalances.test.ts
+++ packages/sdk/src/reads/getVoteLockedConvexTokenLockedBalances.test.ts
@@ -2,12 +2,19 @@ import { publicClientMainnet } from "../../tests/globals.js";
 import { getVoteLockedConvexTokenLockedBalances } from "./getVoteLockedConvexTokenLockedBalances.js";
 import { assert, expect, test } from "vitest";
 
-test("get liquity troves should work correctly", async () => {
+test("get vote locked convex token locked balances should work correctly", async () => {
+  const voteLockedConvexToken = "0x72a19342e8F1838460eBFCCEf09F6585e32db86E" as const;
+  const positionAddress = "0x09602d67d2db8166ad38add363f025f5c8feaad9" as const;
+
   const result = await getVoteLockedConvexTokenLockedBalances(publicClientMainnet, {
-    voteLockedConvexToken: "0x72a19342e8F1838460eBFCCEf09F6585e32db86E",
-    positionAddress: "0x09602d67d2db8166ad38add363f025f5c8feaad9",
+    voteLockedConvexToken,
+    positionAddress,
   });
 
   assert(result !== undefined && result !== null);
   expect(Object.keys(result).length).toBeGreaterThan(0);
+
+  expect(result.total).toBeTypeOf("bigint");
+  expect(result.unlockable).toBeTypeOf("bigint");
+  expect(result.locked).toBeTypeOf("bigint");
 });
