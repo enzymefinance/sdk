96ba2a84d2b81ed12ac1a8fbd24c0deb3e6298ee
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-06 15:54:37 +0200

    feat: add convert crv to cvx test

---
+++ packages/sdk/src/reads/convertCrvToCvxWithCvxMining.test.ts
@@ -0,0 +1,17 @@
+import { publicClientMainnet } from "../../tests/globals.js";
+import { convertCrvToCvxWithCvxMining } from "./convertCrvToCvxWithCvxMining.js";
+import { assert, expect, test } from "vitest";
+
+test("convert crv to cvx with cvx mining should work correctly", async () => {
+  const cvxMining = "0x3c75BFe6FbfDa3A94E7E7E8c2216AFc684dE5343" as const;
+  const amount = 1000n as const;
+
+  const result = await convertCrvToCvxWithCvxMining(publicClientMainnet, {
+    cvxMining,
+    amount,
+  });
+
+  assert(result !== undefined && result !== null);
+
+  expect(result).toBeTypeOf("bigint");
+});
