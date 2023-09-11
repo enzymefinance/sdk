676dfd4079bf1406da898fcee6d559f23530448e
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-11 10:32:13 +0200

    feat: update return object

--- packages/sdk/src/reads/getConvexWrapperEstimateRewards.ts
+++ packages/sdk/src/reads/getConvexWrapperEstimateRewards.ts
@@ -1,8 +1,8 @@
-import { type ReadContractParameters, readContractParameters } from "../utils/viem.js";
 import { IConvexCurveLpStakingWrapperLib } from "@enzymefinance/abis/IConvexCurveLpStakingWrapperLib";
 import { type Address, ContractFunctionExecutionError, type PublicClient } from "viem";
+import { type ReadContractParameters, readContractParameters } from "../utils/viem.js";
 
-export function getConvexWrapperEstimateRewards(
+export async function getConvexWrapperEstimateRewards(
   client: PublicClient,
   args: ReadContractParameters<{
     assetAddress: Address;
@@ -10,13 +10,18 @@ export function getConvexWrapperEstimateRewards(
   }>,
 ) {
   try {
-    return client.simulateContract({
+    const [claimedAmounts, rewardTokens] = (await client.simulateContract({
       ...readContractParameters(args),
       abi: IConvexCurveLpStakingWrapperLib,
       functionName: "claimRewardsFor",
       address: args.assetAddress,
       args: [args.claimAddress],
-    });
+    })) as unknown as [bigint[], Address[]];
+
+    return {
+      claimedAmounts,
+      rewardTokens,
+    };
   } catch (error) {
     // TODO: More selectively catch this error here.
     if (error instanceof ContractFunctionExecutionError) {
