9dfa747bebe60b10e83b13f16efbd31b67e649d7
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-12 13:38:26 +0200

    fix: output object

--- packages/sdk/src/reads/getSpecificAssetsRedemptionExpectedAmounts.ts
+++ packages/sdk/src/reads/getSpecificAssetsRedemptionExpectedAmounts.ts
@@ -5,7 +5,7 @@ import { type Address, ContractFunctionExecutionError, type PublicClient } from 
 export async function getSpecificAssetsRedemptionExpectedAmounts(
   client: PublicClient,
   args: ReadContractParameters<{
-    signerAddress: Address;
+    signer: Address;
     recipient: Address;
     sharesQuantity: bigint;
     payoutAssets: Address[];
@@ -13,17 +13,24 @@ export async function getSpecificAssetsRedemptionExpectedAmounts(
   }>,
 ) {
   try {
-    const {
-      result: [payoutAmounts],
-    } = await client.simulateContract({
+    const { result: payoutAmounts } = await client.simulateContract({
       ...readContractParameters(args),
       abi: IComptrollerLib,
       functionName: "redeemSharesForSpecificAssets",
-      address: args.signerAddress,
+      address: args.signer,
       args: [args.recipient, args.sharesQuantity, args.payoutAssets, args.payoutPercentages],
     });
 
-    return { payoutAmounts };
+    const output: Record<Address, bigint> = {};
+    for (let i = 0; i < args.payoutAssets.length; i++) {
+      // rome-ignore lint/style/noNonNullAssertion: <explanation>
+      const payoutAsset = args.payoutAssets[i]!;
+      // rome-ignore lint/style/noNonNullAssertion: <explanation>
+      const payoutAmount = payoutAmounts[i]!;
+      output[payoutAsset] = payoutAmount;
+    }
+
+    return output;
   } catch (error) {
     // TODO: More selectively catch this error here.
     if (error instanceof ContractFunctionExecutionError) {
