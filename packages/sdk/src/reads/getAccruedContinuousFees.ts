aa4b4b675dfb2eb2173025b408a486276b8bc29b
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-13 11:11:15 +0200

    feat: add json abi and batch calls

--- packages/sdk/src/reads/getAccruedContinuousFees.ts
+++ packages/sdk/src/reads/getAccruedContinuousFees.ts
@@ -2,7 +2,57 @@ import { type ReadContractParameters, readContractParameters } from "../utils/vi
 import { IComptrollerLib } from "@enzymefinance/abis";
 import { IPerformanceFee } from "@enzymefinance/abis";
 import { IUnpermissionedActionsWrapper } from "@enzymefinance/abis/IUnpermissionedActionsWrapper";
-import { type Address, type PublicClient, parseAbi } from "viem";
+import type { Address, PublicClient } from "viem";
+
+const managementAbi = {
+  name: "settle",
+  type: "function",
+  inputs: [
+    {
+      internalType: "address",
+      name: "accessor",
+      type: "address",
+    },
+    {
+      internalType: "address",
+      name: "vaultProxy",
+      type: "address",
+    },
+    {
+      internalType: "uint8",
+      name: "feeType",
+      type: "uint8",
+    },
+    {
+      internalType: "bytes",
+      name: "feeData",
+      type: "bytes",
+    },
+    {
+      internalType: "uint256",
+      name: "gav",
+      type: "uint256",
+    },
+  ],
+  outputs: [
+    {
+      internalType: "uint256",
+      name: "sharesDue_",
+      type: "uint256",
+    },
+    {
+      internalType: "string",
+      name: "reason",
+      type: "string",
+    },
+    {
+      internalType: "uint256",
+      name: "sharesTransferred_",
+      type: "uint256",
+    },
+  ],
+  stateMutability: "view",
+} as const;
 
 export async function getAccruedContinuousFees(
   client: PublicClient,
@@ -18,68 +68,66 @@ export async function getAccruedContinuousFees(
     };
   }>,
 ) {
-  const continuousFees = (await client.readContract({
+  const continuousFees = await client.readContract({
     ...readContractParameters(args),
     abi: IUnpermissionedActionsWrapper,
     functionName: "getContinuousFeesForFund",
     address: args.unpermissionedActionsWrapper,
     args: [args.accessor],
-  })) as unknown as Address[];
+  });
 
   const hasManagementFee = continuousFees.some((fee) => fee === args.contracts.managementFee);
   const hasPerformanceFee = continuousFees.some((fee) => fee === args.contracts.performanceFee);
 
-  const managementAbi = parseAbi([
-    "function settle(address,address,uint8,bytes,uint256) view returns (bigitn, string, bigint)",
-  ] as const);
   let managementFeeSharesDue = 0n;
 
   if (hasManagementFee) {
-    const managementFeeSettledReturn = (await client.readContract({
+    const [sharesDue, _, __] = await client.readContract({
       ...readContractParameters(args),
-      abi: managementAbi,
+      abi: [managementAbi],
       functionName: "settle",
       address: args.accessor,
       args: [args.accessor, args.vaultProxy, 0, "0x", 0n],
-    })) as unknown as {
-      settlementType_: bigint;
-      "1": string;
-      sharesDue_: bigint;
-    };
+    });
 
-    managementFeeSharesDue = managementFeeSettledReturn.sharesDue_;
+    managementFeeSharesDue = sharesDue;
   }
 
   let performanceFeeSharesDue = 0n;
   let highWaterMark = 0n;
 
   if (hasPerformanceFee) {
-    const gav = (await client.readContract({
+    const { result: gav } = await client.simulateContract({
       ...readContractParameters(args),
       abi: IComptrollerLib,
       functionName: "calcGav",
       address: args.accessor,
-    })) as unknown as bigint;
+    });
 
-    const performanceFeeSettledReturn = (await client.readContract({
-      ...readContractParameters(args),
-      abi: IPerformanceFee,
-      functionName: "settle",
-      address: args.contracts.managementFee,
-      args: [args.accessor, args.vaultProxy, 0, "0x", gav],
-    })) as unknown as [number, Address, bigint];
+    const [
+      {
+        result: [_, __, performanceSharesDue],
+      },
+      feeInfoForFund,
+    ] = await Promise.all([
+      client.simulateContract({
+        ...readContractParameters(args),
+        abi: IPerformanceFee,
+        functionName: "settle",
+        address: args.contracts.managementFee,
+        args: [args.accessor, args.vaultProxy, 0, "0x", gav],
+      }),
+      client.readContract({
+        ...readContractParameters(args),
+        abi: IPerformanceFee,
+        functionName: "getFeeInfoForFund",
+        address: args.contracts.performanceFee,
+        args: [args.accessor],
+      }),
+    ]);
 
-    performanceFeeSharesDue = performanceFeeSettledReturn[2];
-
-    const performanceFeeHighWaterMarkReturn = (await client.readContract({
-      ...readContractParameters(args),
-      abi: IPerformanceFee,
-      functionName: "getFeeInfoForFund",
-      address: args.contracts.performanceFee,
-      args: [args.accessor],
-    })) as unknown as [bigint, bigint];
-
-    highWaterMark = performanceFeeHighWaterMarkReturn[1];
+    performanceFeeSharesDue = performanceSharesDue;
+    highWaterMark = feeInfoForFund.highWaterMark;
   }
 
   return {
