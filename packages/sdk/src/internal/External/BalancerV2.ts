6269525732658a3f33091aa3fc1827805d7e6f4b
Ivan Herger <36133712+iherger@users.noreply.github.com>
2023-10-04 15:29:55 +0200

    feat: external calls for Balancer (#190)

--- packages/sdk/src/internal/External/BalancerV2.ts
+++ packages/sdk/src/internal/External/BalancerV2.ts
@@ -1,24 +1,39 @@
 import { Viem } from "@enzymefinance/sdk/Utils";
 import { type Address, type PublicClient } from "viem";
 
-const mintAbi = {
-  inputs: [{ internalType: "address", name: "gauge", type: "address" }],
-  name: "mint",
-  outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-  stateMutability: "nonpayable",
-  type: "function",
-} as const;
+const minterAbi = [
+  {
+    inputs: [{ internalType: "address", name: "gauge", type: "address" }],
+    name: "mint",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
+
+const gaugeAbi = [
+  {
+    inputs: [
+      { internalType: "address", name: "user", type: "address" },
+      { internalType: "address", name: "rewardToken", type: "address" },
+    ],
+    name: "claimable_reward",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
 
 export async function getMinterRewards(
   client: PublicClient,
   args: Viem.ContractCallParameters<{
+    minter: Address;
     beneficiary: Address;
     gauge: Address;
-    minter: Address;
   }>,
 ) {
   const { result } = await Viem.simulateContract(client, args, {
-    abi: [mintAbi],
+    abi: minterAbi,
     functionName: "mint",
     address: args.minter,
     args: [args.gauge],
@@ -26,4 +41,20 @@ export async function getMinterRewards(
   });
 
   return result;
+}
+
+export async function getClaimableRewards(
+  client: PublicClient,
+  args: Viem.ContractCallParameters<{
+    gauge: Address;
+    user: Address;
+    rewardToken: Address;
+  }>,
+) {
+  return Viem.readContract(client, args, {
+    abi: gaugeAbi,
+    functionName: "claimable_reward",
+    address: args.gauge,
+    args: [args.user, args.rewardToken],
+  });
 }
