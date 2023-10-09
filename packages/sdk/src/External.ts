5416d5290a9a0449814903987609503e34fdcded
Ivan Herger <36133712+iherger@users.noreply.github.com>
2023-10-09 09:22:23 +0200

    feat: external calls for delegate votes (#207)

--- packages/sdk/src/External.ts
+++ packages/sdk/src/External.ts
@@ -4,6 +4,7 @@ export * as CompoundV2 from "@enzymefinance/sdk/internal/External/CompoundV2";
 export * as CompoundV3 from "@enzymefinance/sdk/internal/External/CompoundV3";
 export * as Convex from "@enzymefinance/sdk/internal/External/Convex";
 export * as Curve from "@enzymefinance/sdk/internal/External/Curve";
+export * as DelegateVotes from "@enzymefinance/sdk/internal/External/DelegateVotes";
 export * as IdleV4 from "@enzymefinance/sdk/internal/External/IdleV4";
 export * as Liquity from "@enzymefinance/sdk/internal/External/AaveV2";
 export * as Maple from "@enzymefinance/sdk/internal/External/Maple";
