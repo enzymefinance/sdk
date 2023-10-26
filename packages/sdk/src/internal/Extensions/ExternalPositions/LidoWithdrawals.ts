3ef5202e759dacb1a1670f6258f79aa82622c814
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-26 11:12:25 +0200

    feat: add lido withdrawals (#235)

---
+++ packages/sdk/src/internal/Extensions/ExternalPositions/LidoWithdrawals.ts
@@ -0,0 +1,73 @@
+import * as ExternalPositionManager from "@enzymefinance/sdk/internal/ExternalPositionManager";
+import { type Hex, decodeAbiParameters, encodeAbiParameters } from "viem";
+
+export type Action = typeof Action[keyof typeof Action];
+export const Action = {
+  RequestWithdrawals: 0n,
+  ClaimWithdrawals: 1n,
+} as const;
+
+export const create = ExternalPositionManager.createOnly;
+
+//--------------------------------------------------------------------------------------------
+// REQUEST WITHDRAWALS
+//--------------------------------------------------------------------------------------------
+
+export const requestWithdrawals = ExternalPositionManager.makeUse(Action.RequestWithdrawals, requestWithdrawalsEncode);
+export const createAndRequestWithdrawals = ExternalPositionManager.makeCreateAndUse(
+  Action.RequestWithdrawals,
+  requestWithdrawalsEncode,
+);
+
+const requestWithdrawalsEncoding = [
+  {
+    name: "amounts",
+    type: "uint256[]",
+  },
+] as const;
+
+export type RequestWithdrawalsArgs = {
+  amounts: ReadonlyArray<bigint>;
+};
+
+export function requestWithdrawalsEncode(args: RequestWithdrawalsArgs): Hex {
+  return encodeAbiParameters(requestWithdrawalsEncoding, [args.amounts]);
+}
+
+export function requestWithdrawalsDecode(encoded: Hex): RequestWithdrawalsArgs {
+  const [amounts] = decodeAbiParameters(requestWithdrawalsEncoding, encoded);
+
+  return { amounts };
+}
+
+//--------------------------------------------------------------------------------------------
+// CLAIM WITHDRAWALS
+//--------------------------------------------------------------------------------------------
+
+export const claimWithdrawals = ExternalPositionManager.makeUse(Action.ClaimWithdrawals, claimWithdrawalsEncode);
+
+const claimWithdrawalsEncoding = [
+  {
+    name: "requestIds",
+    type: "uint256[]",
+  },
+  {
+    name: "hints",
+    type: "uint256[]",
+  },
+] as const;
+
+export type ClaimWithdrawalsArgs = {
+  requestIds: ReadonlyArray<bigint>;
+  hints: ReadonlyArray<bigint>;
+};
+
+export function claimWithdrawalsEncode(args: ClaimWithdrawalsArgs): Hex {
+  return encodeAbiParameters(claimWithdrawalsEncoding, [args.requestIds, args.hints]);
+}
+
+export function claimWithdrawalsDecode(encoded: Hex): ClaimWithdrawalsArgs {
+  const [requestIds, hints] = decodeAbiParameters(claimWithdrawalsEncoding, encoded);
+
+  return { requestIds, hints };
+}
