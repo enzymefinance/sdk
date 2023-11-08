005f38164f93406dc43258b33e27fdb347958a95
Sebastian Lorenz <fubhy@fubhy.com>
2023-11-08 10:36:45 +0100

    chore: update build

--- examples/example-api-node/src/types.ts
+++
@@ -1,4 +0,0 @@
-import { createGrpcTransport } from "@connectrpc/connect-node";
-
-// TODO: This type should be exported by connect-node.
-export type GrpcTransportOptions = Parameters<typeof createGrpcTransport>[0];
--- examples/example-api-web/.env.tpl
+++
@@ -1,2 +0,0 @@
-# Copy this file to `.env` and add your api token.
-VITE_ENZYME_API_TOKEN=
