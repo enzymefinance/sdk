a18b6492e220e77355b2ceae04abdf26a8bb2585
Ivan Herger <ivan@herger.org>
2023-11-15 08:42:50 +0100

    feat: add Uniswap v3 pool liquidity

--- packages/sdk/src/internal/External/UniswapV3.ts
+++ packages/sdk/src/internal/External/UniswapV3.ts
@@ -10,42 +10,21 @@ const v3PoolAbi = [
     inputs: [],
     name: "slot0",
     outputs: [
-      {
-        internalType: "uint160",
-        name: "sqrtPriceX96",
-        type: "uint160",
-      },
-      {
-        internalType: "int24",
-        name: "tick",
-        type: "int24",
-      },
-      {
-        internalType: "uint16",
-        name: "observationIndex",
-        type: "uint16",
-      },
-      {
-        internalType: "uint16",
-        name: "observationCardinality",
-        type: "uint16",
-      },
-      {
-        internalType: "uint16",
-        name: "observationCardinalityNext",
-        type: "uint16",
-      },
-      {
-        internalType: "uint8",
-        name: "feeProtocol",
-        type: "uint8",
-      },
-      {
-        internalType: "bool",
-        name: "unlocked",
-        type: "bool",
-      },
+      { internalType: "uint160", name: "sqrtPriceX96", type: "uint160" },
+      { internalType: "int24", name: "tick", type: "int24" },
+      { internalType: "uint16", name: "observationIndex", type: "uint16" },
+      { internalType: "uint16", name: "observationCardinality", type: "uint16" },
+      { internalType: "uint16", name: "observationCardinalityNext", type: "uint16" },
+      { internalType: "uint8", name: "feeProtocol", type: "uint8" },
+      { internalType: "bool", name: "unlocked", type: "bool" },
     ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "liquidity",
+    outputs: [{ internalType: "uint128", name: "", type: "uint128" }],
     stateMutability: "view",
     type: "function",
   },
@@ -80,6 +59,19 @@ export async function getSlot0(
     feeProtocol,
     unlocked,
   };
+}
+
+export function getLiquidity(
+  client: PublicClient,
+  args: Viem.ContractCallParameters<{
+    pool: Address;
+  }>,
+) {
+  return Viem.readContract(client, args, {
+    abi: v3PoolAbi,
+    functionName: "liquidity",
+    address: args.pool,
+  });
 }
 
 //--------------------------------------------------------------------------------------------
