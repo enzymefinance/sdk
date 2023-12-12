ad36c46a74481ae45b67c79f567a3e999a7e448b
Ivan Herger <ivan@herger.org>
2023-12-12 08:39:37 +0100

    chore: shares wrapper cleanup

--- packages/sdk/src/Tools.ts
+++ packages/sdk/src/Tools.ts
@@ -1,4 +1,2 @@
 export * as GatedRedemptionQueueSharesWrapper from "./Tools/GatedRedemptionQueueSharesWrapper.js";
 export * as SharesSplitter from "./Tools/SharesSplitter.js";
-export * as SharesWrapper from "./Tools/SharesWrapper.js";
-export * as SharesWrapperFactory from "./Tools/SharesWrapperFactory.js";
