7b1e4504cc4e55ddba6ab67fc5d1baaaadda43b6
Gabriel Rocheleau <contact@rockwaterweb.com>
2024-07-11 13:33:57 -0400

    chore: remove subgraph overwrite

--- packages/environment/src/deployments/ethereum.ts
+++ packages/environment/src/deployments/ethereum.ts
@@ -368,7 +368,7 @@ export default defineDeployment<Deployment.ETHEREUM>({
   subgraphs: {
     assets: { slug: "asset-universe", id: "4ZW3mDNgpDVy68RipQLJxvRw1FReJTfvA7nbB52J4Gjg" },
     balances: { slug: "vault-balances", id: "HwR7jTExHWNvQetTxRYEMQ5hywHyUkierAYvnGS7pBUS" },
-    core: { slug: "enzyme-core-dev", id: "9DLBBLep5UyU16kUQRvxBCMqko4q9XzuE4XsMMpARhKK", devVersion: "ep-alice-v4" },
+    core: { slug: "enzyme-core", id: "9DLBBLep5UyU16kUQRvxBCMqko4q9XzuE4XsMMpARhKK", devVersion: "version/latest" },
     shares: { slug: "vault-shares", id: "6p2L2gQ4Hw4Dh2kxZFDJbcqtbv44vrJbrBEh3EjS7qVo" },
     vaults: { slug: "vault-lineage", id: "5FdivFcUPmVSqCFkv3jqJh3QYjHjh1ztzd7GHiCAMP1h" },
   },
