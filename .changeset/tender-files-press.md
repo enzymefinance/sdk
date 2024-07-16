4cea83ab75c6e0e7747c9fd6c229ff08a5d3f424
Ivan Herger <ivan@herger.org>
2024-07-16 09:08:40 +0200

    fix: remove Alice protocol

---
+++ .changeset/tender-files-press.md
@@ -0,0 +1,5 @@
+---
+"@enzymefinance/environment": patch
+---
+
+Remove Alice protocol
--- packages/environment/src/protocols.ts
+++ packages/environment/src/protocols.ts
@@ -1,7 +1,6 @@
 export enum Protocol {
   AAVE = "AAVE",
   AAVE_V3 = "AAVE_V3",
-  ALICE = "ALICE",
   AURA = "AURA",
   BALANCER = "BALANCER",
   COMPOUND_V2 = "COMPOUND_V2",
