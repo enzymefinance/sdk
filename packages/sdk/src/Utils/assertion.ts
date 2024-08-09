5e1611295d52baf4c9a3e43961b13982704a6808
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-08-09 16:26:35 +0200

    feat: add gmx v2

--- packages/sdk/src/Utils/assertion.ts
+++ packages/sdk/src/Utils/assertion.ts
@@ -7,3 +7,12 @@ export function invariant(condition: any, description: string): asserts conditio
     throw new Error(description);
   }
 }
+
+export function assertEnumType<TObject extends Record<string, number>>(
+  enumType: TObject,
+  value: number,
+): asserts value is TObject[keyof TObject] {
+  if (!Object.values(enumType).includes(value as TObject[keyof TObject])) {
+    throw new Error(`Incorret type: ${value}`);
+  }
+}
