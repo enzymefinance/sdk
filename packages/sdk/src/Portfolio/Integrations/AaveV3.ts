2b7683386ca8d399ec29f761ce2139dd5c1fb86f
Ivan Herger <ivan@herger.org>
2024-10-14 14:38:30 +0200

    feat: aave v3 updates

--- packages/sdk/src/Portfolio/Integrations/AaveV3.ts
+++ packages/sdk/src/Portfolio/Integrations/AaveV3.ts
@@ -610,9 +610,23 @@ export async function getAvailableSupplyAmount(
   args: Viem.ContractCallParameters<{
     protocolDataProvider: Address;
     asset: Address;
+    decimals: number;
   }>,
 ) {
   const [reserveCaps, reserveData] = await Promise.all([getReserveCaps(client, args), getReserveData(client, args)]);
 
-  return reserveCaps.supplyCap - reserveData.totalAToken;
+  return parseUnits(reserveCaps.supplyCap.toString(), args.decimals) - reserveData.totalAToken;
+}
+
+export async function getAvailableVariableDebtAmount(
+  client: Client,
+  args: Viem.ContractCallParameters<{
+    protocolDataProvider: Address;
+    asset: Address;
+    decimals: number;
+  }>,
+) {
+  const [reserveCaps, reserveData] = await Promise.all([getReserveCaps(client, args), getReserveData(client, args)]);
+
+  return parseUnits(reserveCaps.borrowCap.toString(), args.decimals) - reserveData.totalVariableDebt;
 }
