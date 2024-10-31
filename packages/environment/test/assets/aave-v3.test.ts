e5c138de0227ab51c7328bf0985306aa72607124
Ivan Herger <ivan@herger.org>
2024-10-31 19:29:13 +0100

    chore: cleanup

--- packages/environment/test/assets/aave-v3.test.ts
+++ packages/environment/test/assets/aave-v3.test.ts
@@ -2,10 +2,7 @@ import { expect, test } from "vitest";
 
 import { AssetType } from "../../src/index.js";
 import { getClient } from "../utils/client.js";
-import {
-  getUnderlyingAssetAddressLowerCase,
-  getUnderlyingAssetAddressUpperCase,
-} from "../utils/contracts/AaveToken.js";
+import { getUnderlyingAssetAddressUpperCase } from "../utils/contracts/AaveToken.js";
 import { environment } from "../utils/fixtures.js";
 
 const client = getClient(environment.network.id);
@@ -14,6 +11,10 @@ const aaveV3Assets = environment.getAssets({ types: [AssetType.AAVE_V3] });
 const assets = environment.getAssets();
 
 test.each(aaveV3Assets)("aave V3 underlying is correct: $symbol ($name): $id", async (asset) => {
+  // check if underlying is correct
+  const checksum = await getUnderlyingAssetAddressUpperCase(client, { asset: asset.id });
+  expect(checksum.toLowerCase(), "Actual underlying asset does not match expected").toBe(asset.underlying);
+
   // Check that the underlying asset exists.
   expect(
     assets.filter((item) => item.id === asset.underlying).length,
@@ -25,14 +26,5 @@ test.each(aaveV3Assets)("aave V3 underlying is correct: $symbol ($name): $id", a
     asset.registered ? environment.getAsset(asset.underlying).registered : true,
     "Underlying asset not registered",
   ).toBe(true);
-
-  const checksum = await getUnderlyingAssetAddressUpperCase(client, { asset: asset.id }).catch(() =>
-    getUnderlyingAssetAddressLowerCase(client, { asset: asset.id }),
-  );
-
-  console.log(checksum);
-
-  expect(checksum.toLowerCase(), "Actual underlying asset does not match expected").toBe(asset.underlying);
 });
-
 test.skip("empty test suite fallback");
