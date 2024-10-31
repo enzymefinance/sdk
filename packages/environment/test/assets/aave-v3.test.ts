732c40327c6c52216bd68fc71df6d16d7a69b3be
Ivan Herger <ivan@herger.org>
2024-10-31 19:27:27 +0100

    chore: fix aave v3 tests

--- packages/environment/test/assets/aave-v3.test.ts
+++ packages/environment/test/assets/aave-v3.test.ts
@@ -1,7 +1,14 @@
 import { expect, test } from "vitest";
 
 import { AssetType } from "../../src/index.js";
+import { getClient } from "../utils/client.js";
+import {
+  getUnderlyingAssetAddressLowerCase,
+  getUnderlyingAssetAddressUpperCase,
+} from "../utils/contracts/AaveToken.js";
 import { environment } from "../utils/fixtures.js";
+
+const client = getClient(environment.network.id);
 
 const aaveV3Assets = environment.getAssets({ types: [AssetType.AAVE_V3] });
 const assets = environment.getAssets();
@@ -18,6 +25,14 @@ test.each(aaveV3Assets)("aave V3 underlying is correct: $symbol ($name): $id", a
     asset.registered ? environment.getAsset(asset.underlying).registered : true,
     "Underlying asset not registered",
   ).toBe(true);
+
+  const checksum = await getUnderlyingAssetAddressUpperCase(client, { asset: asset.id }).catch(() =>
+    getUnderlyingAssetAddressLowerCase(client, { asset: asset.id }),
+  );
+
+  console.log(checksum);
+
+  expect(checksum.toLowerCase(), "Actual underlying asset does not match expected").toBe(asset.underlying);
 });
 
 test.skip("empty test suite fallback");
