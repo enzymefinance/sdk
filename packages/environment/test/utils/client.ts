035aa6792af4ac893bfda15192691f4f7ac66e61
Ivan Herger <ivan@herger.org>
2024-12-09 15:36:49 +0100

    feat: add more assets to Base deployment

--- packages/environment/test/utils/client.ts
+++ packages/environment/test/utils/client.ts
@@ -1,15 +1,28 @@
-import { http, createPublicClient } from "viem";
-import { arbitrum, mainnet, polygon } from "viem/chains";
+import { http, type PublicClient, createPublicClient } from "viem";
+import { arbitrum, base, mainnet, polygon } from "viem/chains";
 import { Network } from "../../src/networks.js";
 
 export function getClient(network: Network) {
-  const chain = network === Network.ETHEREUM ? mainnet : network === Network.POLYGON ? polygon : arbitrum;
+  const chain =
+    network === Network.ARBITRUM
+      ? arbitrum
+      : network === Network.BASE
+        ? base
+        : network === Network.POLYGON
+          ? polygon
+          : mainnet;
   const url = `https://${
-    network === Network.ETHEREUM ? "eth" : network === Network.POLYGON ? "polygon" : "arb"
+    network === Network.ARBITRUM
+      ? "arb"
+      : network === Network.BASE
+        ? "base"
+        : network === Network.POLYGON
+          ? "polygon"
+          : "eth"
   }-mainnet.g.alchemy.com/v2/${process.env.VITE_ALCHEMY_API_KEY}`;
 
   return createPublicClient({
     chain,
     transport: http(url, { batch: true }),
-  });
+  }) as PublicClient; // TODO: remove typecasting (currently breaks for the Base network client)
 }
