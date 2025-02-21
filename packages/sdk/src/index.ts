57e8d014481b0434f801aeaa8958da73526732fe
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2025-02-21 16:07:02 +0100

    feat: remove address list

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,4 @@
 export * as Asset from "./Asset.js";
-export * as AddressListRegistry from "./AddressListRegistry.js";
 export * as Configuration from "./Configuration.js";
 export * as Depositor from "./Depositor.js";
 export * as LifeCycle from "./LifeCycle.js";
