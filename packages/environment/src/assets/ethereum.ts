1d585cf815e58107e105793494cbc600e101a6b0
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2025-04-10 12:51:15 +0200

    chore: clean up

--- packages/environment/src/assets/ethereum.ts
+++ packages/environment/src/assets/ethereum.ts
@@ -7394,21 +7394,6 @@ export default defineAssetList(Network.ETHEREUM, [
   {
     symbol: "PT-LBTC-27MAR2025",
     name: "PT Lombard LBTC 27MAR2025",
-    id: "0x83a017b13540a85dbfc056b66eb0b35bf72c09e3",
-    type: AssetType.PENDLE_V2_PT,
-    releases: [sulu],
-    decimals: 8,
-    underlying: "0x8236a87084f8b84306f72007f36f2618a5634494",
-    markets: ["0x70b70ac0445c3ef04e314dfda6caafd825428221"],
-    priceFeed: {
-      type: PriceFeedType.PRIMITIVE_PENDLE_V2,
-      aggregator: "0x83a017b13540a85dbfc056b66eb0b35bf72c09e3",
-      rateAsset: RateAsset.ETH,
-    },
-  },
-  {
-    symbol: "PT-LBTC-27MAR2025",
-    name: "PT Lombard LBTC 27MAR2025",
     id: "0xec5a52c685cc3ad79a6a347abace330d69e0b1ed",
     type: AssetType.PENDLE_V2_PT,
     releases: [sulu],
