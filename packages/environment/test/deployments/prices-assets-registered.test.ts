03e0c2181637af0c9c1c173e4ac7520f95c838e5
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2025-04-17 11:43:22 +0200

    chore: revert

--- packages/environment/test/deployments/prices-assets-registered.test.ts
+++ packages/environment/test/deployments/prices-assets-registered.test.ts
@@ -32,6 +32,7 @@ async function isRegistered(asset: Address) {
 
   return Protocol.isSupportedAsset(client, { valueInterpreter: c, asset });
 }
+
 let prices: Record<Address, number | undefined> = {};
 
 beforeAll(async () => {
