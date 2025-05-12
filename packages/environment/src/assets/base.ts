db0fd18d5cfecc79d2cd08b133a7cac286dc8eef
Ivan Herger <ivan@herger.org>
2025-05-12 10:24:08 +0200

    feat: add bCOIN and wbCOIN

--- packages/environment/src/assets/base.ts
+++ packages/environment/src/assets/base.ts
@@ -247,4 +247,31 @@ export default defineAssetList(Network.BASE, [
       rateAsset: RateAsset.USD,
     },
   },
+  {
+    id: "0xbbcb0356bb9e6b3faa5cbf9e5f36185d53403ac9",
+    name: "Backed Coinbase Global",
+    symbol: "bCOIN",
+    decimals: 18,
+    releases: [sulu],
+    type: AssetType.PRIMITIVE,
+    priceFeed: {
+      type: PriceFeedType.PRIMITIVE_CHAINLINK,
+      aggregator: "0xfb2487ee6e6a1f51d31e64d969c9f437235633f0",
+      rateAsset: RateAsset.USD,
+    },
+  },
+  {
+    symbol: "wbCOIN",
+    name: "Wrapped Backed Coinbase Global",
+    id: "0xdec933e2392ad908263e70a386fbf34e703ffe8f",
+    type: AssetType.ERC_4626,
+    protocol: Erc4626Protocol.BCOIN,
+    releases: [sulu],
+    decimals: 18,
+    underlying: "0xbbcb0356bb9e6b3faa5cbf9e5f36185d53403ac9",
+    priceFeed: {
+      type: PriceFeedType.DERIVATIVE_ERC4626,
+      address: "0x6889790fb10a03bbf9dc86f1bed3219b509f5367",
+    },
+  },
 ]);
