75c1765f5ba7cf4f6b923a52569b57591948ccf3
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2025-07-29 09:35:35 +0200

    chore: remove space

--- packages/environment/src/assets/base.ts
+++ packages/environment/src/assets/base.ts
@@ -167,7 +167,7 @@ export default defineAssetList(Network.BASE, [
   {
     decimals: 18,
     id: "0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22",
-    name: "Coinbase Wrapped Staked ETH ",
+    name: "Coinbase Wrapped Staked ETH",
     releases: [sulu],
     symbol: "cbETH",
     type: AssetType.PRIMITIVE,
