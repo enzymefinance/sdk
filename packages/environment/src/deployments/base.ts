43b8e37870d6a8b435ab1e97aa1c745eeadbac2a
Gabriel Rocheleau <contact@rockwaterweb.com>
2025-10-24 09:59:30 -0400

    feat: add usdc to named tokens

--- packages/environment/src/deployments/base.ts
+++ packages/environment/src/deployments/base.ts
@@ -81,6 +81,7 @@ export default defineDeployment<Deployment.BASE>({
     comp: "0x9e1028f5f1d5ede59748ffcee5532509976840e0",
     dai: "0x50c5725949a6f0c72e6c4a641f24049a917db0cb",
     mln: "0x7c298664bd6582f6f264c2cb5a4b9cc09b6e3889",
+    usdc: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
     usdt: "0xfde4c96c8593536e31f229ea8f37b2ada2699bb2",
     weth: "0x4200000000000000000000000000000000000006",
   },
