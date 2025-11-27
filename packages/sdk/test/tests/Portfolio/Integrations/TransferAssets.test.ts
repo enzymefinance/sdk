ea5c42fd3331e1a5f31fa2b2f6a34064a65c68cb
Ivan Herger <ivan@herger.org>
2025-11-27 08:23:53 +0100

    fix: transfer assets adapter

---
+++ packages/sdk/test/tests/Portfolio/Integrations/TransferAssets.test.ts
@@ -0,0 +1,45 @@
+import { Portfolio, Utils } from "@enzymefinance/sdk";
+import { TestActions, TestSetup } from "@enzymefinance/sdk/test";
+import type { Address } from "viem";
+import { test } from "vitest";
+
+const environment = TestSetup.mainnet();
+
+const vaultOwner = environment.constants.alice;
+const sharesBuyer = environment.constants.bob;
+const transferRecipient = environment.constants.carol;
+const depositAmount = Utils.Conversion.toWei(10);
+
+let comptrollerProxy: Address;
+
+test("create vault", async () => {
+  ({ comptrollerProxy } = await TestActions.createVaultAndBuyShares({
+    environment,
+    vaultOwner,
+    sharesBuyer,
+    depositAmount,
+  }));
+});
+
+test("transfer assets should work correctly", async () => {
+  await environment.send({
+    account: vaultOwner,
+    transaction: Portfolio.Integrations.TransferAssets.transfer({
+      comptrollerProxy,
+      integrationManager: environment.constants.integrationManager,
+      integrationAdapter: environment.constants.transferAssetsAdapter,
+      callArgs: {
+        recipient: transferRecipient,
+        assetAddresses: [environment.constants.weth, environment.constants.weth],
+        assetAmounts: [depositAmount / 2n, depositAmount / 2n],
+      },
+    }),
+  });
+
+  await TestActions.assertBalanceOf({
+    environment,
+    asset: environment.constants.weth,
+    owner: transferRecipient,
+    expected: depositAmount,
+  });
+});
