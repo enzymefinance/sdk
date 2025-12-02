a972129140b0b01b569f7ac4c847d170837a2029
Gabriel Rocheleau <18757482+gabrocheleau@users.noreply.github.com>
2025-12-02 16:31:08 -0500

    feat: bebop maker signature decoding helper  (#836)
    * -chore: changeset
    
    * chore: add swap single selector

--- packages/sdk/src/_internal/IntegrationManager.ts
+++ packages/sdk/src/_internal/IntegrationManager.ts
@@ -20,6 +20,7 @@ export const Selector = {
   TakeMultipleOrders: "0x0e7f692d", // takeMultipleOrders(address,bytes,bytes)
   TakeOrder: "0x03e38a2b", // takeOrder(address,bytes,bytes)
   Transfer: "0x3461917c", // transfer(address,bytes,bytes)
+  SwapSingle: "0xa7a19e00", // swapSingle(address,bytes,bytes)
   Unstake: "0x68e30677", // unstake(address,bytes,bytes)
   UnstakeAndRedeem: "0x8334eb99", // unstakeAndRedeem(address,bytes,bytes)
   Wrap: "0xa5ca2d71", // wrap(address,bytes,bytes)
