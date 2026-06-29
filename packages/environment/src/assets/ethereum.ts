c4f12404091831c036fa2bacbecda5dc393950cd
Ivan Herger <ivan@herger.org>
2026-06-29 07:36:24 +0200

    chore: add SYRUP token

--- packages/environment/src/assets/ethereum.ts
+++ packages/environment/src/assets/ethereum.ts
@@ -8008,4 +8008,15 @@ export default defineAssetList(Network.ETHEREUM, [
       type: PriceFeedType.NONE,
     },
   },
+  {
+    symbol: "SYRUP",
+    name: "Syrup Token",
+    id: "0x643c4e15d7d62ad0abec4a9bd4b001aa3ef52d66",
+    type: AssetType.PRIMITIVE,
+    releases: [],
+    decimals: 18,
+    priceFeed: {
+      type: PriceFeedType.NONE,
+    },
+  },
 ]);
